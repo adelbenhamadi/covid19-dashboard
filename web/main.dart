63a50ebe67feb81fdfa609320aef7dae76d982b8
adelbenhamadi <benhamadi.adel@gmail.com>
2021-02-06 16:31:02 +0100

    no need for firebase stuff for this project

--- web/main.dart
+++ web/main.dart
@@ -7,26 +7,8 @@ import 'package:covid19_web/app_component.template.dart' as ng;
 import 'main.template.dart' as self;
 
 @GenerateInjector(
-  /*routerProviders,*/ routerProvidersHash
-)
+    /*routerProviders,*/ routerProvidersHash)
 final InjectorFactory injector = self.injector$Injector;
 void main() {
-  //Config.appID = CryptoService.CreateCryptoRandomString();
-  try{
-   fb.initializeApp(
-      apiKey: "AIzaSyCVYLDGWsyUu1mf7_LpLq1R8T99qIrlF8M",
-      authDomain: "covid-19-933a7.firebaseapp.com",//covid-19-933a7.web.app
-      //databaseURL: "https://covid-19-933a7.firebaseio.com",
-      projectId: "covid-19-933a7",
-      //storageBucket: "covid-19-933a7.appspot.com",
-      appId: "1:1089294773268:web:c9f775e70209ed4c560b01",
-     
-    );
-    //print("fb.initializeApp called!");
-  } on fb.FirebaseJsNotLoadedException catch (e) {
-    print(e);
-  }finally{
-    runApp(ng.AppComponentNgFactory, createInjector: injector);
-  }
-  
+  runApp(ng.AppComponentNgFactory, createInjector: injector);
 }
